69a6c8b315326b71c98fad86ce5fe23b0d992fe5
Rob Hoelz <rob@hoelz.ro>
2024-05-27 11:48:12 -0500

    Verify target fingerprint

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
+	"net"
 	"os"
 
 	"golang.org/x/crypto/ssh"
@@ -10,6 +12,7 @@ import (
 
 func main() {
 	targetIP := os.Args[1]
+	targetFingerprint := os.Args[2]
 	password := os.Getenv("SSH_PASS")
 
 	config := &ssh.ClientConfig{
@@ -17,7 +20,15 @@ func main() {
 		Auth: []ssh.AuthMethod{
 			ssh.Password(password),
 		},
-		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+		HostKeyCallback: func(hostname string, remote net.Addr, key ssh.PublicKey) error {
+			fingerprint := ssh.FingerprintLegacyMD5(key)
+			// XXX we don't need to do a constant-time comparison, right? And comparing the fingerprint
+			//     should suffice, yeah?
+			if fingerprint != targetFingerprint {
+				return errors.New("fingerprint mismatch")
+			}
+			return nil
+		},
 	}
 
 	client, err := ssh.Dial("tcp", targetIP+":22", config)
